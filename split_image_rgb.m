dfdf08e173cd42131977371faf8903dd1ead5158
Muhammad S Shamim <sa501428@gmail.com>
2015-06-22 01:09:41 -0500

    Update

--- split_image_rgb.m
+++ split_image_rgb.m
@@ -8,8 +8,8 @@ red_image = 0*image; red_image(:,:,1) = image(:,:,1);
 green_image = 0*image; green_image(:,:,2) = image(:,:,2);
 blue_image = 0*image; blue_image(:,:,3) = image(:,:,3);
 
-imwrite(red_image, strcat('red.',image_filename), 'png', 'Alpha', ones(ni, nj));
-imwrite(green_image, strcat('green.',image_filename), 'png', 'Alpha', ones(ni, nj));
-imwrite(blue_image, strcat('blue.',image_filename), 'png', 'Alpha', ones(ni, nj));
+imwrite(red_image, strcat('red.',image_filename), 'png', 'Alpha', 200*ones(ni, nj));
+imwrite(green_image, strcat('green.',image_filename), 'png', 'Alpha', 200*ones(ni, nj));
+imwrite(blue_image, strcat('blue.',image_filename), 'png', 'Alpha', 200*ones(ni, nj));
 
 end
