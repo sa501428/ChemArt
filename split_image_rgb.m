2dc4e9f95a7593fb52d2dd304246aad8f90dd170
Muhammad S Shamim <sa501428@gmail.com>
2015-06-22 00:46:57 -0500

    Images

---
+++ split_image_rgb.m
@@ -0,0 +1,15 @@
+function split_image_rgb(image_filename)
+
+image = imread(image_filename);
+
+[ni, nj, ~] = size(image);
+
+red_image = 0*image; red_image(:,:,1) = image(:,:,1);
+green_image = 0*image; green_image(:,:,2) = image(:,:,2);
+blue_image = 0*image; blue_image(:,:,3) = image(:,:,3);
+
+imwrite(red_image, strcat('red.',image_filename), 'png', 'Alpha', ones(ni, nj));
+imwrite(green_image, strcat('green.',image_filename), 'png', 'Alpha', ones(ni, nj));
+imwrite(blue_image, strcat('blue.',image_filename), 'png', 'Alpha', ones(ni, nj));
+
+end
